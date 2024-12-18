73bf8ee33ba15b4b9d9e1b47e4828a5c81184ea6
Jack Dickinson <jackdickinson090@hotmail.com>
2024-12-18 20:01:14 +0000

    start adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
+  const ankiCode: string = 
 
   return (
     <>
-      <h1>Anki Flash Card Generator</h1>
-      <div></div>
+      <h1 className="mb-10">Anki Flash Card Generator</h1>
+      <div className="grid w-full gap-2">
+        <Textarea
+          onChange={handleOnChange}
+          placeholder="Type your message here."
+          className="mb-4"
+        />
+        <Button className="mb-4">Generate flash card codes</Button>
+      </div>
+      <div>Code to copy to Anki app:</div>
+
+      <div>{text}</div>
     </>
   );
 }
